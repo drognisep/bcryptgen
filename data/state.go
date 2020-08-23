c7f53769abc14c671d2853675fbc77cfc3b7c2d8
Doug Saylor <doug@saylorsolutions.com>
2020-08-22 20:47:37 -0500

    Adding observable for hash

--- data/state.go
+++ data/state.go
@@ -5,5 +5,8 @@ import "fyne.io/fyne"
 // Pass is the observable state of the password entered by the user or generated.
 var Pass *StringSubject = NewStringSubject("")
 
+// Hash is the observable state of the bcrypt hash generated from the password.
+var Hash *StringSubject = NewStringSubject("")
+
 // MainWindow is held as global state to enable easy reference with dialogs.
 var MainWindow fyne.Window
--- ui/bcryptGen.go
+++ ui/bcryptGen.go
@@ -9,9 +9,16 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// NewBcryptField creates a new container with the content initialized.
 func NewBcryptField() *fyne.Container {
 	entry := widget.NewEntry()
-	btn := widget.NewButton("Generate Hash", genBcrypt(entry))
+	btn := widget.NewButton("Generate Hash", genBcrypt)
+	data.Hash.Attach(func(s string) {
+		entry.SetText(s)
+	})
+	data.Pass.Attach(func(s string) {
+		entry.SetText("")
+	})
 
 	content := fyne.NewContainerWithLayout(
 		layout.NewVBoxLayout(),
@@ -26,14 +33,13 @@ func NewBcryptField() *fyne.Container {
 	return content
 }
 
-func genBcrypt(entry *widget.Entry) func() {
-	return func() {
-		pass := data.Pass.GetState()
-		hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
-		if err != nil {
-			dialog.ShowError(err, data.MainWindow)
-			entry.SetText("")
-		}
-		entry.SetText(string(hash))
+func genBcrypt() {
+	pass := data.Pass.GetState()
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		dialog.ShowError(err, data.MainWindow)
+		data.Hash.SetState("")
+		return
 	}
+	data.Hash.SetState(string(hash))
 }
