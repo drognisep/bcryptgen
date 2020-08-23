d6b883bf62bb07534cbaffd53360b44d431fd553
Doug Saylor <doug@saylorsolutions.com>
2020-08-22 22:03:15 -0500

    Adding copy buttons

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 	"github.com/drognisep/bcryptgen/data"
 	"github.com/drognisep/bcryptgen/ui"
@@ -14,10 +15,21 @@ func main() {
 	win.SetMaster()
 	data.MainWindow = win
 	ui.InitErrorMessage(win)
+	copyPassBtn := widget.NewButton("Copy password", func() {
+		win.Clipboard().SetContent(data.Pass.GetState())
+	})
+	copyHashBtn := widget.NewButton("Copy hash", func() {
+		win.Clipboard().SetContent(data.Hash.GetState())
+	})
 
 	win.SetContent(widget.NewVBox(
 		ui.NewPasswordField(),
 		ui.NewBcryptField(),
+		fyne.NewContainerWithLayout(
+			layout.NewHBoxLayout(),
+			copyPassBtn,
+			copyHashBtn,
+		),
 	))
 	win.Resize(fyne.NewSize(640, 300))
 
